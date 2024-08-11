bdd4b893149ae2619cb80f758638cec36a9b8ad2
gitkunal777 <gitkunal777@gmail.com>
2024-08-11 18:05:04 +0000

    Done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# Importing modules
+from cryptography.fernet import Fernet as fnt
 
+# Function(s)
+def encrypt(text):
+ print("")
+ key = fnt.generate_key()
+ encrypted_msg = fnt(key).encrypt(text.encode())
+ print("Key: ",key.decode())
+ print("")
+ print("Encrypted Message: ",encrypted_msg.decode())
+
+# Program
+usr_inp = input('Enter the secrate message: ')
+encrypt(usr_inp)
