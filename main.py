1601ce315ee1495d358bbaa7aa584f348bdba755
gitkunal777 <gitkunal777@gmail.com>
2024-08-12 03:17:57 +0000

    Complete

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ def encrypt(text):
  print("")
  print("Encrypted Message: ",encrypted_msg.decode())
 
+def decrypt(text, key):
+ print("")
+ key = key
+ decrypted_msg = fnt(key).decrypt(text.encode())
+ print("")
+ print("Your Message: ",decrypted_msg.decode())
+
 # Program
-usr_inp = input('Enter the secrate message: ')
-encrypt(usr_inp)
+usr_inp = input('Enter [1] for reciver, [2] for sender: ')
+if usr_inp == '1':
+ print("")
+ key = input("Enter the Key: ")
+ print("")
+ msg = input("Enter the Secrate Message: ")
+ decrypt(msg, key)
+
+else:
+ print("")
+ msg = input("Enter the Message: ")
+ encrypt(msg)
